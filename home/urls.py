1a8947fa8109fcb0198cca5f967c74f9ed46b10c
c-mfy <xinyusu06@gmail.com>
2025-10-15 23:44:30 -0400

    part 4
    + local pop. map

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home.index'),
     path('about', views.about, name='home.about'),
+    path('map', views.map, name='home.map'),
+    path('api/trending', views.trending_movies_api, name='trending_movies_api'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from cart.models import MovieLocationTrend
+
 # Create your views here.
 def index(request):
     template_data = {}
@@ -9,4 +12,29 @@ def about(request):
     template_data = {}
     template_data['title'] = 'About'
     return render(request, 'home/about.html',
-                  {'template_data': template_data})
+                    {'template_data': template_data})
+    
+def map(request):
+    template_data = {'title': 'Local Popularity Map'}
+    return render(request, 'home/map.html', {'template_data': template_data})
+
+def trending_movies_api(request):
+    genre = request.GET.get('genre', None)
+    queryset = MovieLocationTrend.objects.all()
+    if genre:
+        queryset = queryset.filter(movie__genre__iexact=genre)
+    
+    data = []
+    for trend in queryset:
+        data.append({
+            'movie': trend.movie.name,
+            'purchase_count': trend.purchase_count,
+            'location': {
+                'city': getattr(trend, 'city', ''),
+                'state': getattr(trend, 'state', ''),
+                'country': getattr(trend, 'country', ''),
+                'lat': getattr(trend, 'latitude', 0),
+                'lng': getattr(trend, 'longitude', 0),
+            }
+        })
+    return JsonResponse({'results': data})
